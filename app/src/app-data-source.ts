3b9012786f3997934bfdb608cf2d581aa9513784
Dylan Herthoge <dherthoge@asaptire.com>
2023-08-02 14:08:57 -0400

    actual src folder

---
+++ app/src/app-data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from "typeorm"
+import { Order } from "./entity/Order"
+import { Location } from "./entity/Location"
+import { Timeslot } from "./entity/Timeslot"
+import { OrderContact } from "./entity/OrderContact"
+import { db}
+
+export const dataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "password",
+    database: "test",
+    entities: [Location, Order, OrderContact, Timeslot],
+    logging: true,
+    synchronize: true,
+})
