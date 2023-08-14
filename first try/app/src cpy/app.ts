333bb0b3a4e73bd9f54828d28414a1a387e234d5
Dylan Herthoge <dherthoge@asaptire.com>
2023-08-14 15:42:04 -0400

    removed first try

--- first try/app/src cpy/app.ts
+++
@@ -1,7 +0,0 @@
-import * as express from "express"
-import { Request, Response } from "express"
-const app = express()
-// start express server
-app.listen(3000, () => {
-    console.log(`Server is running on port ${3000}.`)
-})
